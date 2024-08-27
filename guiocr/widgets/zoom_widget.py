33a865478565afdbae4b67a2a0b6348dc82e54d0
Songling Huang <98691464+lingskr@users.noreply.github.com>
2024-08-27 11:32:29 +0800

    Add files via upload

---
+++ guiocr/widgets/zoom_widget.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+
+
+class ZoomWidget(QtWidgets.QSpinBox):
+    def __init__(self, value=100):
+        super(ZoomWidget, self).__init__()
+        self.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.setRange(1, 1000)
+        self.setSuffix(" %")
+        self.setValue(value)
+        self.setToolTip("Zoom Level")
+        self.setStatusTip(self.toolTip())
+        self.setAlignment(QtCore.Qt.AlignCenter)
+
+    def minimumSizeHint(self):
+        height = super(ZoomWidget, self).minimumSizeHint().height()
+        fm = QtGui.QFontMetrics(self.font())
+        width = fm.width(str(self.maximum()))
+        return QtCore.QSize(width, height)
